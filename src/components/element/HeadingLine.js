ec4e0077cb7855b82b25ff580418a02aef946376
Parksoyoon <maudlinsy@gmail.com>
2021-07-16 17:15:07 +0900

    FIX HeadingLine hover event error

---
+++ src/components/element/HeadingLine.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+import NextLink from "@/components/element/NextLink";
+
+const Container = styled.div`
+  width: 100%;
+  max-height: 12vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1em;
+  font-family: ${(props) => props.theme.fontEng};
+  color: ${(props) => props.theme.white.color};
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.15);
+  margin-bottom: 1em;
+`;
+
+const Title = styled.span`
+  font-size: 1.2em;
+  font-weight: 600;
+`;
+
+const Text = styled.span`
+  padding: 0.5em 1em;
+  border-radius: 2vw;
+  color: ${(props) => props.theme.white.color};
+  font-size: 0.7em;
+  background-color: ${(props) => props.theme.lightGreen.color};
+`;
+
+function HeadingLine() {
+  return (
+    <Container>
+      <Title>TOP LANK ITEMS</Title>
+      <Text>
+        <NextLink href="/product">
+          MORE..
+        </NextLink>
+      </Text>
+    </Container>
+  );
+}
+
+export default HeadingLine;
